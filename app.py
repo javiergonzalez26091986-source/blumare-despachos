d679daad7fe92ccc230382566e71db3fcbef84e9
javiergonzalez26091986-source <javier.gonzalez26091986@gmail.com>
2026-05-25 22:50:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ else:
 # =============================================================================
 # 2. ENCABEZADO DE LA APP
 # =============================================================================
-st.markdown("<h1 style='text-align: center; color: white; margin-bottom: 0;'>BLUMARE</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #0d1117; margin-bottom: 0;'>BLUMARE</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; color: #0d1117; font-weight: bold; letter-spacing: 2px; margin-top: 0;'>LOGÍSTICA Y DESPACHOS</p>", unsafe_allow_html=True)
 st.markdown("---")
 
